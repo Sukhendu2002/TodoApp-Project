e9fe7c675d9633890dd9df1d09ead5dc64547911
sukhendu2002 <sukhenduguria2017@gmail.com>
2021-10-22 15:35:38 +0530

    add list functionality

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import tempData from "./tempData";
+import TodoList from "./components/todoList";
+import colors from "./Colors";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo
+            <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={24} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+        </View>
+
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            horizontal={true}
+            keyExtractor={(item) => item.name}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => <TodoList list={item} />}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    // backgroundColor: colors.blue,
+    height: 700,
+  },
+  divider: {
+    backgroundColor: colors.blue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.blue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
+    marginLeft: 8,
   },
 });
---
+++ Colors.js
@@ -0,0 +1,8 @@
+export default colors = {
+  black: "#2D3436",
+  white: "#FFFFFF",
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#24A6D9",
+  lightBlue: "#A7CBDS",
+};
